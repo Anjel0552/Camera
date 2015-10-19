211f2eba8d31ac320d26e2365ebd19f67a8faa56
Anjel0552 <anjelvillafranco0552@gmail.com>
2015-10-19 09:26:00 -0400

    initial create

---
+++ Camera/Camera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Camera
+//
+//  Created by Anjel Villafranco on 10/19/15.
+//  Copyright © 2015 Anjel Villafranco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
